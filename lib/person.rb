1b627718c2a30285fa1a3101f6eec54d8db42378
Carrie Kraft <carriekarft@gmail.com>
2023-06-04 20:52:53 -0700

    commit tree 047c75812d1402a80b9c9964332dfd4e7e59f6dc

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
+require "pry"
 class Person
   # your code here
+  # attr_accessor :name, :height, :eye_color
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+
+puts susan.height
+
+# binding.pry 0
